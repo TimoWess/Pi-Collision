3f7924c0e871357d03203d24f90edd2396cbf0d8
Timo Wesselmann <timo.wesselmann@gmail.com>
2022-09-08 17:27:29 +0200

    update: added pure functional collision calculaion

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,14 @@ import Control.Monad (unless, when)
 
 main :: IO ()
 main = do
-    let digits = 5
-        steps = 10000
+    let digits = 8
+        steps = 3000000
         b1 = fromXWMV 100 50 1 0
         b2 = fromXWMV 300 150 (100 ^ digits) (-(5 / steps))
-    update b1 b2 0
+        collisions = functionalUpdate b1 b2 0
+    case collisions of
+        (Left fail) -> putStrLn fail
+        (Right cols) -> print cols
 
 data Block =
     Block Double Double Double Double
@@ -37,12 +40,13 @@ colliding (Block x1 w1 _ _) (Block x2 w2 _ _) =
     not $ x1 + w1 < x2 || x1 > x2 + w2
 
 bounce :: Block -> Block -> (Block, Block)
-bounce b1@(Block x1 w1 ma va) b2@(Block x2 w2 mb vb) =
+bounce (Block x1 w1 ma va) (Block x2 w2 mb vb) =
     (fromXWMV x1 w1 ma n1, fromXWMV x2 w2 mb n2)
   where
     sumM = ma + mb
-    n1 = ((ma - mb) / sumM) * va + ((2 * mb) / sumM) * vb
-    n2 = ((2 * ma) / sumM) * va + ((mb - ma) / sumM) * vb
+    diff = ma - mb
+    n1 = (diff / sumM) * va + ((2 * mb) / sumM) * vb
+    n2 = ((2 * ma) / sumM) * va + ((-diff) / sumM) * vb
 
 update :: Block -> Block -> Integer -> IO ()
 update b1 b2 col = do
@@ -52,17 +56,39 @@ update b1 b2 col = do
                 then bounce b1 b2
                 else (b1, b2)
         hitWall = x1 < 0
-        newCol = col + boolToInteger areColliding + boolToInteger hitWall
-        newB1 =
+        nextCol = col + boolToInteger areColliding + boolToInteger hitWall
+        nextB1 =
             updatePosition
                 (if hitWall
                      then reverseVelocity t1
                      else t1)
-        newB2 = updatePosition t2
+        nextB2 = updatePosition t2
     when (x1 > x2 + w2) $ error "Left block on the wrong side"
     if (v1 >= 0 && v2 >= 0 && v2 >= v1)
-        then print newCol
-        else update newB1 newB2 newCol
+        then print nextCol
+        else update nextB1 nextB2 nextCol
+
+functionalUpdate :: Block -> Block -> Integer -> Either String Integer
+functionalUpdate b1 b2 col =
+    let areColliding = colliding b1 b2
+        (t1@(Block x1 _ _ v1), t2@(Block x2 w2 _ v2)) =
+            if areColliding
+                then bounce b1 b2
+                else (b1, b2)
+        hitWall = x1 < 0
+        nextCol = col + boolToInteger areColliding + boolToInteger hitWall
+        nextB1 =
+            updatePosition
+                (if hitWall
+                     then reverseVelocity t1
+                     else t1)
+        nextB2 = updatePosition t2
+        failed = x1 > x2 + w2
+     in if failed
+            then Left ("Failed calculation at: " ++ show col ++ " collisions!")
+            else if (v1 >= 0 && v2 >= 0 && v2 >= v1)
+                     then Right nextCol
+                     else functionalUpdate nextB1 nextB2 nextCol
 
 boolToInteger :: Bool -> Integer
 boolToInteger = toInteger . fromEnum
